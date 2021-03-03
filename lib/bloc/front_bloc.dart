399f22448c29ae6e36da5c8d3e09544901371739
BuildTools <is713932@iteso.mx>
2021-03-03 00:30:36 -0600

    Feat: Add main functionality to front.dart file

--- lib/bloc/front_bloc.dart
+++ lib/bloc/front_bloc.dart
@@ -11,7 +11,7 @@ class FrontBloc extends Bloc<FrontEvent, FrontState> {
   FrontBloc() : super(FrontInitial());
 
   List<String> list1 = ["Perro", "Gato"];
-  List<String> list2 = ["Perro", "Gato"];
+  List<String> list2 = ["Perros", "Gato"];
   int count = 0;
   int listSize = 0;
   int index = 0;
@@ -21,15 +21,18 @@ class FrontBloc extends Bloc<FrontEvent, FrontState> {
     FrontEvent event,
   ) async* {
     if (event is StartEvent) {
-      int listSize = list1.length;
+      listSize = list1.length;
+      index = 0;
       count = 0;
       yield JuegoInicialState(
           palabra: list1[index], titulo: "The word is...", contador: count);
     } else if (event is SkipEvent) {
+      if (list1[index] != list2[index]) count++;
       index++;
+
+      // verificar que el indice no se sobrepase el tamaño de la lista y que se pueda seguir jugando
       if (index < listSize) {
-        // verificar que el indice no se sobrepase el tamaño de la lista y que se pueda seguir jugando
-        yield JuegoNextState(
+        yield JuegoInicialState(
             palabra: list1[index], titulo: "The word is...", contador: count);
       } else {
         yield JuegoEndState(titulo: "Your scored", contador: count);
@@ -37,15 +40,16 @@ class FrontBloc extends Bloc<FrontEvent, FrontState> {
     } else if (event is GotEvent) {
       if (list1[index] == list2[index]) count++;
       index++;
+
+      // verificar que el indice no se sobrepase el tamaño de la lista y que se pueda seguir jugando
       if (index < listSize) {
-        // verificar que el indice no se sobrepase el tamaño de la lista y que se pueda seguir jugando
-        yield JuegoNextState(
+        yield JuegoInicialState(
             palabra: list1[index], titulo: "The word is...", contador: count);
       } else {
         yield JuegoEndState(titulo: "Your scored", contador: count);
       }
-      yield JuegoNextState(
-          palabra: list1[index], titulo: "The word is...", contador: count);
+      // yield JuegoNextState(
+      //     palabra: list1[index], titulo: "The word is...", contador: count);
     } else if (event is EndEvent) {
       count = 0;
       yield JuegoEndState(titulo: "Your scored", contador: count);
---
+++ lib/front.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/front_bloc.dart';
+
+class Front extends StatefulWidget {
+  Front({Key key}) : super(key: key);
+
+  @override
+  _FrontState createState() => _FrontState();
+}
+
+class _FrontState extends State<Front> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: BlocProvider(
+        create: (context) => FrontBloc(),
+        child: BlocConsumer<FrontBloc, FrontState>(
+          listener: (context, state) {
+            // TODO: implement listener
+          },
+          builder: (context, state) {
+            if (state is JuegoInicialState) {
+              return Container(
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          children: [
+                            Text(state.titulo),
+                            SizedBox(height: 10.0),
+                            Text(
+                              state.palabra,
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: [
+                            Text(state.contador.toString()),
+                            Row(
+                              children: [
+                                MaterialButton(
+                                    child: Text("SKIP"),
+                                    color: Colors.green,
+                                    onPressed: () {
+                                      BlocProvider.of<FrontBloc>(context)
+                                          .add(SkipEvent());
+                                    }),
+                                MaterialButton(
+                                    child: Text("GOT IT"),
+                                    color: Colors.green,
+                                    onPressed: () {
+                                      BlocProvider.of<FrontBloc>(context)
+                                          .add(GotEvent());
+                                    }),
+                                MaterialButton(
+                                    child: Text("END GAME"),
+                                    color: Colors.green,
+                                    onPressed: () {
+                                      BlocProvider.of<FrontBloc>(context)
+                                          .add(EndEvent());
+                                    }),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            } else if (state is JuegoEndState) {
+              return Container(
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          children: [
+                            Text(state.titulo),
+                            SizedBox(height: 10.0),
+                            Text(
+                              state.contador.toString(),
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 10,
+                        child: MaterialButton(
+                            child: Text("PLAY AGAIN"),
+                            color: Colors.green,
+                            onPressed: () {
+                              BlocProvider.of<FrontBloc>(context)
+                                  .add(StartEvent());
+                            }),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            } else
+              return Container(
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Get ready to"),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "Guess the word!",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: MaterialButton(
+                        child: Text(
+                          "PLAY",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        color: Colors.green,
+                        onPressed: () {
+                          BlocProvider.of<FrontBloc>(context).add(StartEvent());
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+          },
+        ),
+      ),
+    );
+  }
+}
